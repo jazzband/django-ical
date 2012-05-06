8f6e4eff7b9f035c961d7fab32daa230b8bc9246
Ian Lewis <IanLewis@users.noreply.github.com>
2012-05-06 18:32:51 +0900

    Added url meta-data to setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup (
     author='Ian Lewis',
     author_email='IanMLewis@gmail.com',
     license='MIT License',
+    url='https://bitbucket.org/IanLewis/django-ical',
     classifiers=[
       'Development Status :: 3 - Alpha',
       'Environment :: Plugins',
