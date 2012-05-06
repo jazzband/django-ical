50f16930ed5837a6d8b851ca17795edb667ea0c1
Ian Lewis <IanLewis@users.noreply.github.com>
2012-05-06 16:49:48 +0900

    Some small changes, doc etc.

--- django_ical/feedgenerator.py
+++ django_ical/feedgenerator.py
@@ -1,11 +1,43 @@
 #:coding=utf-8:
 
+"""
+iCalendar feed generation library -- used for generating
+iCalendar feeds.
+
+Sample usage:
+
+>>> from django_ical import feedgenerator
+>>> from datetime import datetime
+>>> feed = feedgenerator.ICal20Feed(
+...     title=u"My Events",
+...     link=u"http://www.example.com/events.ical",
+...     description=u"A iCalendar feed of my events.",
+...     language=u"en",
+... )
+>>> feed.add_item(
+...     title="Hello",
+...     link=u"http://www.example.com/test/",
+...     description="Testing."
+...     start_datetime=datetime(2012, 5, 6, 10, 00),
+...     end_datetime=datetime(2012, 5, 6, 12, 00),
+... )
+>>> fp = open('test.ical', 'w')
+>>> feed.write(fp, 'utf-8')
+>>> fp.close()
+
+For definitions of the iCalendar format see:
+http://www.ietf.org/rfc/rfc2445.txt
+"""
+
+
+
 from icalendar import Calendar, Event
 
 from django.utils.feedgenerator import SyndicationFeed
 
 __all__ = (
     'ICal20Feed',
+    'DefaultFeed',
 )
 
 FEED_FIELD_MAP = (
@@ -17,7 +49,7 @@ FEED_FIELD_MAP = (
 )
 
 ITEM_EVENT_FIELD_MAP = (
-    # item_guid は unique_id になる
+    # 'item_guid' becomes 'unique_id' when passed to the SyndicationFeed
     ('unique_id',           'uid'),
     ('title',               'summary'),
     ('description',         'description'),
@@ -32,9 +64,16 @@ ITEM_EVENT_FIELD_MAP = (
 )
 
 class ICal20Feed(SyndicationFeed):
+    u"""
+    iCalendar 2.0 Feed implementation.
+    """
     mime_type='text/calendar; charset=utf8'
 
     def write(self, outfile, encoding):
+        u"""
+        Writes the feed to the specified file in the
+        specified encoding.
+        """
         cal = Calendar()
         cal.add('version', '2.0')
         cal.add('calscale', 'GREGORIAN')
@@ -62,3 +101,5 @@ class ICal20Feed(SyndicationFeed):
                 if val is not None:
                     event.add(efield, val)
             calendar.add_component(event)
+
+DefaultFeed = ICal20Feed
