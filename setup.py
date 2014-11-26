bb374ffab36965fc421c169550ca32e4fe9cdc55
Ian Lewis <IanMLewis@gmail.com>
2014-11-26 09:59:56 +0900

    Updated setup.py to comply with PEP-8 better

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
- 
-setup (
+
+setup(
     name='django-ical',
     version='1.2',
     description="iCal feeds for Django based on Django's syndication feed framework.",
@@ -12,15 +12,15 @@ setup (
     license='MIT License',
     url='https://bitbucket.org/IanLewis/django-ical',
     classifiers=[
-      'Development Status :: 3 - Alpha',
-      'Environment :: Plugins',
-      'Framework :: Django',
-      'Intended Audience :: Developers',
-      'License :: OSI Approved :: BSD License',
-      'Programming Language :: Python',
-      'Topic :: Software Development :: Libraries :: Python Modules',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Plugins',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    install_requires = [
+    install_requires=[
         'Django>=1.3.4',
         'icalendar>=3.1',
     ],
