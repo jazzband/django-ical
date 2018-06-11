dac119868229a3cd7d6cee3ef2afe405229823d9
Christian Ledermann <christian.ledermann@gmail.com>
2018-06-11 16:59:41 +0100

    add tests for recurrences

--- django_ical/utils.py
+++ django_ical/utils.py
@@ -6,8 +6,13 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 # 3rd party
-from dateutil.rrule import FREQNAMES
 from icalendar.prop import vRecur
+
+
+def build_rrule_from_text(rrule_str):
+    """Build an rrule from a serialzed RRULE string."""
+    recurr = vRecur()
+    return recurr.from_ical(rrule_str)
 
 
 def build_rrule_from_recurrences_rrule(rule):
@@ -22,8 +27,7 @@ def build_rrule_from_recurrences_rrule(rule):
     line = serialize(rule)
     if line.startswith('RRULE:'):
         line = line[6:]
-    recurr = vRecur()
-    return recurr.from_ical(line)
+    return build_rrule_from_text(line)
 
 
 def build_rrule_from_dateutil_rrule(rule):
@@ -38,12 +42,13 @@ def build_rrule_from_dateutil_rrule(rule):
     for line in lines:
         if line.startswith('DTSTART:'):
             continue
-        recurr = vRecur()
-        return recurr.from_ical(line)
+        return build_rrule_from_text(line)
 
 
-def build_rrule(count=None, interval=None, bysecond=None, byminute=None, byhour=None, byweekno=None,
-                bymonthday=None, byyearday=None, bymonth=None, until=None, bysetpos=None, wkst=None, byday=None, freq=None):
+def build_rrule(count=None, interval=None, bysecond=None, byminute=None,
+                byhour=None, byweekno=None, bymonthday=None, byyearday=None,
+                bymonth=None, until=None, bysetpos=None, wkst=None, byday=None,
+                freq=None):
     """
     Builds rrule dictionary for vRecur class
     :param count: int
