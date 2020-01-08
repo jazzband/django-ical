1d12f75df960e2d40e723a49fa35cafcf1480504
Aleksi Häkli <aleksi.hakli@iki.fi>
2020-01-08 20:04:05 +0200

    Add Django 3.0 support

--- tests.py
+++ tests.py
@@ -1,40 +1,38 @@
 import os
 import sys
-import django
 
 
 def main():
     """
-    Standalone django model test with a 'memory-only-django-installation'.
+    Standalone Django model test with a 'memory-only-django-installation'.
     You can play with a django model without a complete django app installation.
     http://www.djangosnippets.org/snippets/1044/
     """
-    os.environ["DJANGO_SETTINGS_MODULE"] = "django.conf.global_settings"
-    from django.conf import global_settings
 
-    global_settings.SECRET_KEY = "snakeoil"
-    global_settings.TIME_ZONE = "UTC"
+    import django
+    from django.conf import settings
 
-    global_settings.INSTALLED_APPS = ("django.contrib.contenttypes", "django_ical")
-    global_settings.DATABASES = {
-        "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}
-    }
-
-    global_settings.MIDDLEWARE_CLASSES = (
-        "django.middleware.common.CommonMiddleware",
-        "django.contrib.sessions.middleware.SessionMiddleware",
+    settings.configure(
+        DATABASES={
+            "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}
+        },
+        INSTALLED_APPS=["django.contrib.contenttypes", "django_ical"],
+        MIDDLEWARE_CLASSES=[
+            "django.middleware.common.CommonMiddleware",
+            "django.contrib.sessions.middleware.SessionMiddleware",
+        ],
+        SECRET_KEY="snakeoil",
+        TIME_ZONE="UTC",
     )
 
-    if django.VERSION > (1, 7):
-        django.setup()
+    django.setup()
 
     from django.test.utils import get_runner
 
-    test_runner = get_runner(global_settings)
-
-    test_runner = test_runner()
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner()
     failures = test_runner.run_tests(["django_ical"])
-    sys.exit(failures)
+    sys.exit(bool(failures))
 
 
 if __name__ == "__main__":
