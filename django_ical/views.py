869a15ea9dd306bfb7f1d09dc3c5b381d5904154
Aleksi Häkli <aleksi.hakli@iki.fi>
2021-05-03 17:17:26 +0300

    Fix Content-Type headers
    HTTP standard only allows one type for the header
    
    Fixes #48

--- django_ical/views.py
+++ django_ical/views.py
@@ -78,10 +78,10 @@ class ICalFeed(Feed):
             obj = self.get_object(request, *args, **kwargs)
         except ObjectDoesNotExist:
             raise Http404("Feed object does not exist.")
+
         feedgen = self.get_feed(obj, request)
-        response = HttpResponse(
-            content_type="text/calendar, text/x-vcalendar, application/hbs-vcs"
-        )
+        response = HttpResponse(content_type=feedgen.mime_type)
+
         if hasattr(self, "item_pubdate") or hasattr(self, "item_updateddate"):
             # if item_pubdate or item_updateddate is defined for the feed, set
             # header so as ConditionalGetMiddleware is able to send 304 NOT MODIFIED
